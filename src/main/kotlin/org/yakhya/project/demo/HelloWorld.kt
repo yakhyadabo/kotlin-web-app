69d4c7627940e25b06d29d636d2dcc53842b984a
Yakhya Dabo <yakhyadabo@gmail.com>
2017-10-14 13:34:12 +0100

    Initial commit

---
+++ src/main/kotlin/org/yakhya/project/demo/HelloWorld.kt
@@ -0,0 +1,5 @@
+package org.yakhya.project.demo
+
+fun main(args : Array<String>) {
+  println("Hello, world!")
+}
---
+++ src/test/kotlin/org/yakhya/project/demo/HelloWorld.kt
@@ -0,0 +1,10 @@
+package org.yakhya.project.demo
+
+import kotlin.test.assertEquals
+import org.junit.Test as test
+
+class TestSource() {
+  @test fun f() {
+    assertEquals("Hello", "Hello")
+  }
+}
