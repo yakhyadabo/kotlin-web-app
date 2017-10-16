df935604f918a1d69cc4070dc2d6a0a27568c602
Yakhya Dabo <yakhyadabo@gmail.com>
2017-10-16 17:27:20 +0100

    [all] Refactor gradle.build files

--- kotlin-repository/src/main/kotlin/org/yakhya/project/kotlin/repository/UserRepository.kt
+++ kotlin-repository/src/main/kotlin/org/yakhya/project/kotlin/repository/UserRepository.kt
@@ -1,2 +1,11 @@
 package org.yakhya.project.kotlin.repository
 
+import org.yakhya.project.kotlin.domain.User
+
+class UserRepository {
+  fun findUser(id: String) = User(id, password = "############")
+}
+
+fun main(args : Array<String>){
+  TODO("Later")
+}
--- src/main/kotlin/org/yakhya/project/demo/HelloWorld.kt
+++
@@ -1,5 +0,0 @@
-package org.yakhya.project.demo
-
-fun main(args : Array<String>) {
-  println("Hello, world!")
-}
