9f8eba2b2df93d11bf40558978767952b583a880
Yakhya Dabo <yakhyadabo@gmail.com>
2018-05-15 22:26:10 +0100

    [kotlin-repository] Add simple DAO method

--- kotlin-repository/src/main/kotlin/org/yakhya/project/kotlin/repository/UserRepository.kt
+++ kotlin-repository/src/main/kotlin/org/yakhya/project/kotlin/repository/UserRepository.kt
@@ -1,11 +1,38 @@
 package org.yakhya.project.kotlin.repository
 
+import org.jooq.impl.DSL
 import org.yakhya.project.kotlin.domain.User
 
-class UserRepository {
-  fun findUser(id: String) = User(id, password = "############")
-}
 
-fun main(args : Array<String>){
-  TODO("Later")
+class UserRepository {
+
+  private val dsl = DSL.using("jdbc:postgresql://localhost:5432/iris-db", "iris-user", "iris-password")!!
+  private val a = Tables.IRIS_USER!!
+  private val b = Tables.USER_PROFILE!!
+
+  fun findUser(id: Int):User{
+    return dsl.select(a.ID, a.LOGIN, a.PASSWORD)
+        .from(a)
+        .join(b).on(a.ID.eq(b.IRIS_USER_ID))
+        .where(a.ID.equal(id))
+        .fetchOneInto(User::class.java)
+
+  }
+
+  fun all(): List<User> {
+
+    return dsl.select(a.ID, a.LOGIN, a.PASSWORD)
+        .from(a)
+        .join(b).on(a.ID.eq(b.IRIS_USER_ID))
+        .orderBy(1, 2, 3)
+        .fetchInto(User::class.java)
+  }
+
+  fun printAll(){
+    dsl.select(a.ID, a.LOGIN, a.PASSWORD, b.USER_PROFILE_TYPE_ID)
+        .from(a)
+        .join(b).on(a.ID.eq(b.IRIS_USER_ID))
+        .orderBy(1, 2, 3)
+        .forEach { println("${it[b.USER_PROFILE_TYPE_ID]} by ${it[a.ID]} ${it[a.LOGIN]} ${it[a.PASSWORD]}") }
+  }
 }
