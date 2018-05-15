ac9edfea58309682ad011c18f6d58bb1a7d0793e
Yakhya Dabo <yakhyadabo@gmail.com>
2018-05-15 22:25:45 +0100

    [kotlin-domain] Use simple constructor in User

--- kotlin-domain/src/main/kotlin/org/yakhya/project/kotlin/domain/User.kt
+++ kotlin-domain/src/main/kotlin/org/yakhya/project/kotlin/domain/User.kt
@@ -1,3 +1,4 @@
 package org.yakhya.project.kotlin.domain
 
-data class User(val login: String, val password: String, val roles: Set<Role> = emptySet())
+data class User(val id: Int, val login: String, val password: String)
+//data class User(val id: String, val login: String, val password: String, val roles: Set<Role> = emptySet())
