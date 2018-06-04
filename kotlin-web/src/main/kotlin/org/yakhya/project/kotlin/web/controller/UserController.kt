0ff80acae5d023e42ef81247fe1088238520961a
Yakhya Dabo <yakhyadabo@gmail.com>
2018-06-04 22:02:45 +0100

    [kotlin-web] @Update(userID)

--- kotlin-web/src/main/kotlin/org/yakhya/project/kotlin/web/controller/UserController.kt
+++ kotlin-web/src/main/kotlin/org/yakhya/project/kotlin/web/controller/UserController.kt
@@ -1,14 +1,17 @@
 package org.yakhya.project.kotlin.web.controller
 
 import org.jooby.Request
+import org.jooby.Result
+import org.jooby.Results
 import org.jooby.mvc.GET
+import org.jooby.mvc.POST
 import org.jooby.mvc.Path
 import org.yakhya.project.kotlin.domain.User
 import org.yakhya.project.kotlin.repository.UserRepository
 import javax.inject.Inject
 import javax.inject.Named
 
-@Path("/api")
+@Path("/api/v1")
 class UserController @Inject constructor(private val userRepository: UserRepository, val req: Request) {
 
   @GET
@@ -24,6 +27,13 @@ class UserController @Inject constructor(private val userRepository: UserReposit
   }
 
 
+  @POST
+  @Path("/user")
+  fun update(@Named("id") userId: Int): Result {
+    req.flash("success", "The item has been created")
+    return Results.json(userRepository.findUser(userId))
+
+  }
 
 
 }
