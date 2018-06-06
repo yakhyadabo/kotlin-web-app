0c49ff2bcc5f92d6ee9f72fdacae77f38802026d
Yakhya Dabo <yakhyadabo@gmail.com>
2018-06-06 22:22:28 +0100

    [kotlin-web] POST upload file

--- kotlin-web/src/main/kotlin/org/yakhya/project/kotlin/web/controller/UserController.kt
+++ kotlin-web/src/main/kotlin/org/yakhya/project/kotlin/web/controller/UserController.kt
@@ -1,16 +1,15 @@
 package org.yakhya.project.kotlin.web.controller
 
-import org.jooby.Err
-import org.jooby.Kooby
-import org.jooby.Status
-import org.jooby.require
-import org.jooby.Results
+import org.jooby.*
 import org.jooby.apitool.ApiTool
+import org.slf4j.LoggerFactory
 import org.yakhya.project.kotlin.repository.UserRepository
 import org.yakhya.project.kotlin.web.module.UserModule
 
 // See: https://github.com/jooby-project/apitool-kotlin-starter/blob/master/src/main/kotlin/starter/apitool/App.kt
 // See: https://dev.to/edgarespina/jooby-export-your-javakotlin-api-to-swaggerraml-45g9
+
+internal val COMPLETED = "Completed"
 
 class UserController : Kooby({
 
@@ -55,5 +54,29 @@ class UserController : Kooby({
 
       user
     }
+
+    /**
+     * Upload users from CSV file
+     *
+     * @return Returns `200` with a single user or `404`
+     */
+    post("/upload") { req, res ->
+      val upload = req.file("sampleFile")
+
+      val file = upload.file()
+
+      log.info("Script Uploaded file size: " + file.length())
+
+      upload.close()
+
+      res.send(COMPLETED)
+
+    }
   }
 })
+
+{
+  companion object {
+    private val log = LoggerFactory.getLogger(UserController::class.java)
+  }
+}
