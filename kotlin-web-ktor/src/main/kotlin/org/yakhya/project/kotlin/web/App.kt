6a17b1cae9164fdfae55698adfc941b67e9c03e9
Yakhya Dabo <yakhyadabo@gmail.com>
2018-06-13 18:52:42 +0100

    [kotlin-web-ktor] Start web server

--- kotlin-web-ktor/src/main/kotlin/org/yakhya/project/kotlin/web/App.kt
+++ kotlin-web-ktor/src/main/kotlin/org/yakhya/project/kotlin/web/App.kt
@@ -1,9 +1,52 @@
 package org.yakhya.project.kotlin.web
 
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.Compression
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import java.text.DateFormat
+import java.time.LocalDate
 
-/**
- * Run application:
- */
+data class Model(val name: String, val items: List<Item>, val date: LocalDate = LocalDate.of(2018, 4, 13))
+data class Item(val key: String, val value: String)
+
+val model = Model("root", listOf(Item("A", "Apache"), Item("B", "Bing")))
+
 fun main(args: Array<String>) {
 
+  embeddedServer(Netty, 8082) {
+
+    install(DefaultHeaders)
+    install(Compression)
+    install(CallLogging)
+    install(ContentNegotiation) {
+      gson {
+        setDateFormat(DateFormat.LONG)
+        setPrettyPrinting()
+      }
+    }
+
+    routing {
+      get("/v1") {
+        call.respond(model)
+      }
+      get("/v1/item/{key}") {
+        val item = model.items.firstOrNull { it.key == call.parameters["key"] }
+        if (item == null)
+          call.respond(HttpStatusCode.NotFound)
+        else
+          call.respond(item)
+      }
+    }
+  }.start(wait = true)
+  
 }
